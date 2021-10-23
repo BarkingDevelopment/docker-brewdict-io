fa255e031ce3ccc9f1c6c9cce5c906b5babaac78
Max Barker <max@the-barkers.com>
2021-10-23 12:53:22 +0000

    Created Jenkins docker image using Code as Configuration (casc).

---
+++ jcasc-blueocean/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Builds the jcasc-blueocean image.
+
+docker build -t jcasc-blueocean:1.0 .
---
+++ jcasc-blueocean/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# Runs the jcasc-blueocean image in a Docker container.
+
+docker run --name jcasc-blueocean --rm --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --env JENKINS_ADMIN_ID=admin --env JENKINS_ADMIN_PASSWORD=password \
+  --publish 8080:8080 --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+   jcasc-blueocean:1.0
