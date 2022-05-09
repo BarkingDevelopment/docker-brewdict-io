462947eb00ab6fa846a47cebc466577f0ad21cb5
Max Barker <max@the-barkers.com>
2022-05-09 08:13:11 +0000

    Committing forgotten portainer folder

---
+++ portainer/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Builds the portainer image.
+
+docker build -t mlbarker/portainer:1.0 -t mlbarker/portainer:latest .
+
---
+++ portainer/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Runs the portainer image in a Docker container.
+
+docker run mlbarker/portainer:1.0
